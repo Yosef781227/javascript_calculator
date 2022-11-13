944e2a579d4ffd7b7de3ffc777d8ec969011495c
Yosef781227 <yosefnakachew101@gmail.com>
2022-11-13 04:15:24 -0800

    javascript_calc

--- src/App.js
+++ src/App.js
@@ -2,83 +2,101 @@ import './App.css';
 import {useState}  from 'react'
 
 function App() {
+       const [calc,setCalac] = useState("")
+       const [result,setResult] = useState("")
 
-  const [Display,setDisplay] = useState('0')
-
-  const AC = () =>{
-        const number = 0;
-         setDisplay(number)
-  }
-
-  const handlenumber = (event) =>{
-      const number = event.target.textContent;
-      if(Display == '0'){
-            if(number ==  '0'){
-                event.stopPropagation()
-            }else{
-              if(number == '.'){
-                  setDisplay(prev => prev + number); 
-              }else{
-                  setDisplay(number); 
-              }
-            }
-      }
-    else{
-      setDisplay(Display+number)
-    }
-     
-
-  }
-
-  const handleopreator = (event) =>{
-      const opreator = event.target.textContent
-      setDisplay(Display + ' ' + opreator + ' ')
-  }
-
-  const  handleequal = () =>{
-
-       setDisplay(eval(Display));
-
-  }
-
-  const handledecimal = () =>{
-      // const array = Display.split('');
-      // const lastEleme = array[array.length - 1];
+       const ops = ['/','*', '+', '-', '.', ];
       
-      // if(!lastEleme.includes('.')){
-      //    setDisplay(Display+'.')
-      // }
+
+        
+       const updateCalac = (value) =>{
+       
+       if(
+        ops.includes(value) && calc === ' ' ||
+        ops.includes(value) && ops.includes(calc.slice(-1)
+        )
+       ){
+             return ;
+        }
+
+        if(calc == '0'){
+              if(value ==  '0'){
+                  value.stopPropagation()
+              }else{
+                if(value == '.'){
+                    setCalac(prev => prev + value); 
+                }else{
+                    setCalac(value); 
+                }
+              }
+        }
+      else{
+        setCalac(calc+value)
+      }
+       }
+
+
+  const createDigits = () => {
+          const digits = [];
+          const arr =['zero','one','two','three','four','five','six','seven','eight','nine']
+        
+          for (let i = 0; i  <=9;  i++){
+            console.log(arr[i])
+            digits.push(
+              <button id={arr[i]}   onClick={ () => updateCalac(i.toString())}>{i}</button>
+            )
+          }
+          return digits
+  }
+
+  const calculate = () =>{
+      setResult(eval(calc).toString());
+      setCalac(eval(calc).toString());
+  }
+
+  const deleteL = () =>{
+     if(calc === ' '){
+      return;
+     }
+     const value = calc.slice(0, -1);
+     const v= setCalac (value);
+     setResult(v)
+  }
+  const allclear = () =>{
+    const num=0
+    setCalac(num);
+    setResult(num);
   }
 
   return (
     <div className="App">
+          <div className="calculator">
+                <div className="display" id ='display'>
+                     {result ? <span>({result})</span> : ""}
+                     &nbsp;{calc || "0"}
+                      <div className="opreators">
+                                <button id="divide"  onClick={ () => updateCalac('/')}>/</button>
+                                <button id="multiply" onClick={ () => updateCalac('*')}>*</button>
+                                <button id='add' onClick={ () => updateCalac('+')}>+</button>
+                                <button id="subtract" onClick={ () => updateCalac('-')}>-</button>
+                                <button id='clear' onClick={allclear}>C</button>
+                                <button id='clear' onClick={deleteL}>AC</button>
+                      </div>
+                      <div className="digits">
+                                  {createDigits()}
+                                
+                                <button id='decimal' onClick={ () => updateCalac('.')}>.</button>
+                                <button onClick={calculate} id ="equals">=</button>
+                      </div>
 
-      
-  <div className="calculator">
-  <div id="display" className="row">
-    {Display}
-  </div>
-  <div id="clear" className="row" onClick={AC}>
-    AC
-  </div>
-  <div id="seven" onClick={handlenumber}>7</div>
-  <div id="eight" onClick={handlenumber}>8</div>
-  <div id="nine"onClick={handlenumber}>9</div>
-  <div id="four" onClick={handlenumber}>4</div>
-  <div id="five"  onClick={handlenumber} >5</div>
-  <div id="six"  onClick={handlenumber}>6</div>
-  <div id="one"  onClick={handlenumber}>1</div>
-  <div id="two"  onClick={handlenumber}>2</div>
-  <div id="three"  onClick={handlenumber}>3</div>
-  <div id="zero"  onClick={handlenumber}>0</div>
-  <div id="multiply" onClick={handleopreator}>*</div>
-  <div id="add" onClick={handleopreator}>+</div>
-  <div id="divide" onClick={handleopreator}>/</div>
-  <div id="decimal" onClick={handlenumber}>.</div>
-  <div id="equals" onClick={handleequal}>=</div>
-  <div id="subtract" onClick={handleopreator}>-</div>
-</div>;
+                    
 
+                </div>
+          </div>
+          <div>
+                              
+                      </div>
+   
     </div>
   );
 }
